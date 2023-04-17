1a5c39dd3fcc88b5c0078fdad220e1c4e759917d
Seiry Yu <4397354+seiry@users.noreply.github.com>
2023-04-18 02:47:40 +0800

    replace esphome to seiry

--- docker/generate_tags.py
+++ docker/generate_tags.py
@@ -55,11 +55,11 @@ def main():
 
     with open(os.environ["GITHUB_OUTPUT"], "w") as f:
         print(f"channel={channel}", file=f)
-        print(f"image=esphome/esphome{suffix}", file=f)
+        print(f"image=seiry/esphome{suffix}", file=f)
         full_tags = []
 
         for tag in tags_to_push:
-            full_tags += [f"esphome/esphome{suffix}:{tag}"]
+            full_tags += [f"seiry/esphome{suffix}:{tag}"]
         print(f"tags={','.join(full_tags)}", file=f)
 
 
